0df1b7d4affefcc6c225d4102851a5c5ad365114
Ankit Arsh <ankitarsh19@gmail.com>
2025-03-22 01:48:49 +0530

    Migrated new project structure and added frontend code based on react

--- src/main/java/com/arsh/project/secure_messaging/enums/MessageStatus.java
+++
@@ -1,8 +0,0 @@
-package com.arsh.project.secure_messaging.enums;
-
-//An enum to represent the values for message status
-public enum MessageStatus {
-    SENT,
-    READ,
-    DELIVERED
-}
--- src/main/java/com/arsh/project/secure_messaging/enums/MessageType.java
+++
@@ -1,9 +0,0 @@
-package com.arsh.project.secure_messaging.enums;
-
-//An enum to represent the values for message type
-public enum MessageType {
-    TEXT,
-    IMAGE,
-    VIDEO,
-    AUDIO
-}
